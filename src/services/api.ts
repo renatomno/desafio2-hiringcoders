bcc450cedbb3364eb3213da99ed3f70b0df79b89
Renato Oliveira <51255373+renatomno@users.noreply.github.com>
2021-07-25 16:52:24 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8888/produtos'
+  baseURL: 'https://my-json-server.typicode.com/renatomno/desafio2-hiringcoders/produtos'
 })
 
 export default api;
